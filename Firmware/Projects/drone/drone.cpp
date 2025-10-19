4d5e35c173b877c334ab626d7931d2cc493176a9
Marco Spataro <marcospataro03@gmail.com>
2025-10-19 22:36:47 +0200

    Add audio model test

--- Firmware/Projects/drone/drone.cpp
+++ Firmware/Projects/drone/drone.cpp
@@ -7,44 +7,78 @@
 using namespace daisy;
 using namespace daisysp;
 
-DaisySeed hw;
+constexpr size_t AUDIO_BLOCK_SIZE = 32;
 
-void AudioCallback(AudioHandle::InputBuffer in, AudioHandle::OutputBuffer out, size_t size)
-{
-	using namespace math;
+CpuLoadMeter audio_load_meter;
+DcBlock audio_dcblock;
+volatile static float param_pot = 0.0;
+volatile static float freq_pot = 0.0;
 
-	static Rossler model;
-	static vec3f state;
+void AudioCallback(AudioHandle::InputBuffer in, AudioHandle::OutputBuffer out, size_t block_size);
 
-	for (size_t i = 0; i < size; i++)
-	{
-		vec3f next = model.step(state);
-		state = next;
-		float val = state.z();
+enum AdcChannels {
+	ParamPot = 0,
+	FreqPot,
+	NUM_CHANNELS
+};
 
-		out[0][i] = val;
-		out[1][i] = val;
-	}
-}
+int main(void) {
+	DaisySeed hw;
 
-int main(void)
-{
 	hw.Init();
-	hw.SetAudioBlockSize(4); // number of samples handled per callback
+	hw.StartLog();
+
+	// Audio 
+	hw.SetAudioBlockSize(AUDIO_BLOCK_SIZE);
 	hw.SetAudioSampleRate(SaiHandle::Config::SampleRate::SAI_48KHZ);
 	hw.StartAudio(AudioCallback);
 
-	math::vec3f test1{10.0f, -1.0f, 2.4f}, test2{0.1f, 2.0f, -0.7f};
-	test1 + test2;
+	audio_load_meter.Init(hw.AudioSampleRate(), hw.AudioBlockSize());
 
-	math::Henon henz;
-	henz.step({0.1, 0.3});
+	audio_dcblock.Init(hw.AudioSampleRate());
 
-	math::Halvorsen halv;
-	(void) halv;
+	// ADC
+	AdcChannelConfig adc_config[AdcChannels::NUM_CHANNELS];
+	adc_config[AdcChannels::ParamPot].InitSingle(seed::A0);
+	adc_config[AdcChannels::FreqPot].InitSingle(seed::A1);
 
-	math::vec<3, double>::Base a;
-	(void) a;
+	hw.adc.Init(adc_config, AdcChannels::NUM_CHANNELS);
+	hw.adc.Start();
 
-	while(1) {}
+	// Main loop
+	hw.PrintLine("End system init");
+
+	while(1) {
+		param_pot = hw.adc.GetFloat(AdcChannels::ParamPot);
+		freq_pot = hw.adc.GetFloat(AdcChannels::FreqPot);
+		// hw.PrintLine("Param: " FLT_FMT3 " -- Freq: " FLT_FMT3, FLT_VAR3(param_pot), FLT_VAR3(freq_pot));
+
+		// float cpu_load = audio_load_meter.GetAvgCpuLoad() * 100.f;
+		// hw.PrintLine("CPU load: " FLT_FMT(2) "%", FLT_VAR(2, cpu_load));
+
+		System::Delay(100);
+	}
+}
+
+void AudioCallback(AudioHandle::InputBuffer in, AudioHandle::OutputBuffer out, size_t block_size) {
+	using namespace math;
+
+	static Halvorsen model;
+	static vec3f state{1.0, 0.0, 1.0};
+
+	audio_load_meter.OnBlockStart();
+
+	for (size_t i = 0; i < block_size; i++) {
+		model.a = 1.4f + param_pot * 1.6f;
+		model.dt = 0.005f + freq_pot * 0.01f;
+
+		state = model.step(state);
+		float val = state.z();
+
+		val = audio_dcblock.Process(val) / 100.f;
+		out[0][i] = val;
+		out[1][i] = val;
+	}
+
+	audio_load_meter.OnBlockEnd();
 }
